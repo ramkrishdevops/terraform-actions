0b698eb9e85ce771359c333ae807537b9b38cce1
Ramakrishnan <ramakrishnan@AGLS-MLT-1312.local>
2025-10-08 11:55:33 +0530

    test

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,11 @@
 # EC2 Instance
-resource "aws_instance" "myec2" {
+resource "aws_instance" "myec233" {
   ami                    = var.ec2_ami
   instance_type          = var.instance_type
  # vpc_security_group_ids = [var.security_group_id]
   associate_public_ip_address = true
 
   tags = merge(var.tags, { Name = "${var.name_prefix}-ec2" })
+
 }
 
